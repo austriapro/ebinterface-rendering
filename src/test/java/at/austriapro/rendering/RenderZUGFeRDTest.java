24194166ad618dd838d7e6f4459df95c7bb6a975
paundl <grafro1988@gmail.com>
2015-11-06 16:57:44 +0100

    REFACTORED pom.xml -> removed dependency itext (was not used)

--- src/test/java/at/austriapro/rendering/RenderZUGFeRDTest.java
+++ src/test/java/at/austriapro/rendering/RenderZUGFeRDTest.java
@@ -1,6 +1,7 @@
 package at.austriapro.rendering;
 
 
+import java.awt.Desktop;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.junit.Test;
@@ -18,67 +19,71 @@ import at.austriapro.rendering.util.OSDetector;
  */
 public class RenderZUGFeRDTest {
 
-  private static boolean OPENFILES = true;
+    private static boolean OPENFILES = true;
 
-  private static final Logger LOG = LoggerFactory.getLogger(RenderZUGFeRDTest.class.getName());
+    private static final Logger LOG = LoggerFactory.getLogger(RenderZUGFeRDTest.class.getName());
 
-  @Test
-  public void testJasperRenderer() throws Exception {
-    ZugferdRenderer zRenderer = new ZugferdRenderer();
+    @Test
+    public void testJasperRenderer() throws Exception {
+        ZugferdRenderer zRenderer = new ZugferdRenderer();
 
-    InputStream isReport = RenderZUGFeRDTest.class.getClassLoader()
-        .getResourceAsStream(
-            "reports/zugferd_sample.jrxml");
+        InputStream isReport =
+            RenderZUGFeRDTest.class.getClassLoader()
+                .getResourceAsStream("reports/zugferd_sample.jrxml");
 
-    InputStream isXML = RenderZUGFeRDTest.class.getClassLoader()
-        .getResourceAsStream(
-            "xml/zugferd.xml");
+        InputStream
+            isXML =
+            RenderZUGFeRDTest.class.getClassLoader().getResourceAsStream("xml/zugferd_demo.xml");
 
-    InputStream logo = RenderZUGFeRDTest.class.getClassLoader()
-        .getResourceAsStream(
-            "logos/logo.jpg");
+        InputStream
+            logo =
+            RenderZUGFeRDTest.class.getClassLoader().getResourceAsStream("logos/logo.jpg");
 
-    byte[]
-        zugferdPdf =
-        zRenderer.renderReport(IOUtils.toByteArray(isReport), IOUtils.toByteArray(isXML), logo);
+        byte[]
+            zugferdPdf =
+            zRenderer.renderReport(IOUtils.toByteArray(isReport), IOUtils.toByteArray(isXML), logo);
 
-    File file = File.createTempFile("zugferd", ".pdf");
-    FileUtils.writeByteArrayToFile(file, zugferdPdf);
+        File file = File.createTempFile("zugferd", ".pdf");
+        FileUtils.writeByteArrayToFile(file, zugferdPdf);
 
-    LOG.info("File written to " + file.getAbsolutePath());
+        LOG.info("File written to " + file.getAbsolutePath());
 
-    openFile(file);
+        openFile(file);
 
-  }
-
-
-  /**
-   * Opens a file, if the operating system is either windows or mac (= local development)
-   */
-  private boolean openFile(File file) {
-
-    // change the flag for easier local development of pdf files
-    if (OPENFILES == false) {
-      return false;
     }
 
-    try {
-      if (OSDetector.isWindows()) {
-        Runtime.getRuntime().exec(new String[]
-                                      {"rundll32", "url.dll,FileProtocolHandler",
+
+    /**
+     * Opens a file, if the operating system is either windows or mac (= local development)
+     */
+    private boolean openFile(File file) {
+
+        // change the flag for easier local development of pdf files
+        if (OPENFILES == false) {
+            return false;
+        }
+
+        try {
+            if (OSDetector.isWindows()) {
+                Runtime.getRuntime()
+                    .exec(new String[]{"rundll32", "url.dll,FileProtocolHandler",
                                        file.getAbsolutePath()});
-        return true;
-      } else if (OSDetector.isMac()) {
-        Runtime.getRuntime().exec(new String[]{"/usr/bin/open",
-                                               file.getAbsolutePath()});
-        return true;
-      } else {
-        return false;
-      }
-    } catch (Exception e) {
-      LOG.error(e.getMessage(), e);
-      return false;
+                return true;
+            } else if (OSDetector.isMac()) {
+                Runtime.getRuntime().exec(new String[]{"/usr/bin/open", file.getAbsolutePath()});
+                return true;
+
+            } else if (OSDetector.isLinux()) {
+                java.awt.Desktop.getDesktop().open(file);
+                return true;
+
+            } else {
+                return false;
+            }
+        } catch (Exception e) {
+            LOG.error(e.getMessage(), e);
+            return false;
+        }
     }
-  }
 
 }
