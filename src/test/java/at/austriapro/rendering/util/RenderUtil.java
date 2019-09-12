68c835df09805a05c3936998f8b98b2105a38385
Philip Helger <philip@helger.com>
2019-09-12 13:37:19 +0200

    Less warnings

--- src/test/java/at/austriapro/rendering/util/RenderUtil.java
+++ src/test/java/at/austriapro/rendering/util/RenderUtil.java
@@ -1,16 +1,17 @@
 package at.austriapro.rendering.util;
 
 import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by paul on 12/11/15.
  */
 public class RenderUtil {
 
-  /**
+  /*
    * Opens a file, if the operating system is either windows or mac (= local development)
    */
-  public static void openFile(File file) throws Exception {
+  public static void openFile(File file) throws IOException {
     if (OSDetector.isWindows()) {
       Runtime.getRuntime()
           .exec(new String[]{"rundll32", "url.dll,FileProtocolHandler",
